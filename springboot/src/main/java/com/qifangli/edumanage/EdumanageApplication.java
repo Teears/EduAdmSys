82ed941274c9fdfda33a0d759b46e6a69ee8abd0
Teears <2734189761@qq.com>
2020-04-26 00:37:11 +0800

    添加后台

---
+++ springboot/src/main/java/com/qifangli/edumanage/EdumanageApplication.java
@@ -0,0 +1,13 @@
+package com.qifangli.edumanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumanageApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/qifangli/edumanage/EdumanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qifangli.edumanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdumanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
